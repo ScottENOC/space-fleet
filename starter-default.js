b1a8cdfbec86ed1eda967a6df49ff39c9c6e77b8
ScottENOC <bales.scott@gmail.com>
2026-09-13 12:35:30 +1000

    v72 connect ordinary encounters to mystery evidence

--- starter-default.js
+++ starter-default.js
@@ -3,6 +3,7 @@ import './mobile-ui.js?v=66';
 import './battle-context-ui.js?v=65';
 import './pdc-system.js?v=66';
 import './mystery-ui.js?v=72';
+import './mystery-encounter-hooks.js?v=72';
 
 if(!document.querySelector('link[href*="mystery-ui.css"]')){
   const link=document.createElement('link');link.rel='stylesheet';link.href='mystery-ui.css?v=72';document.head.append(link);
